62a2e7f5ba706cc383f6d91a1cfb618d43d20deb
Ryan Hieber <floatingpoint@gmail.com>
2026-01-30 01:37:19 -0800

    Misc. whitespace cleanup

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,6 +1,6 @@
-import UIKit
-import SwiftUI
 import ComposeApp
+import SwiftUI
+import UIKit
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
@@ -17,6 +17,3 @@ struct ContentView: View {
             .ignoresSafeArea()
     }
 }
-
-
-
